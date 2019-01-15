6d1b2470c8fe2d6006937c35984d25f790435778
Brian <bredow.brian@gmail.com>
2019-01-15 10:24:44 +0100

    print  the JS output to HTML

--- calculate Age/assets/main.js
+++ calculate Age/assets/main.js
@@ -3,16 +3,19 @@ function calculateAge (animal, animalAge){
     if (animal == 'dog'){
      let n = (animalAge + 7);
       console.log('Your '+ animal +' is ' + n + ' years old in human years');
-      document.getElementById('calcAge');
+      document.getElementById('calcAge').innerHTML='Your '+ animal +' is ' + n + ' years old in human years';
     } else if (animal == 'cow'){
      let n = animalAge + 14;
-      console.log('Your cow is ' + n + 'years old in human years')
+      console.log('Your cow is ' + n + ' years old in human years');
+      document.getElementById('calcAge').innerHTML='Your '+ animal +' is ' + n + ' years old in human years';
     } else if (animal == 'chinchilla'){
      let n = animalAge + 25;
-      console.log('Your chinchilla is ' + n + ' years old in human years')
+      console.log('Your chinchilla is ' + n + ' years old in human years');
+      document.getElementById('calcAge').innerHTML='Your '+ animal +' is ' + n + ' years old in human years';
     } else {
-      console.log('?')
+      console.log('I dont know that animal, Sorry!')
+      document.getElementById('calcAge').innerHTML='Sorry, I dont  know that animal yet. Please try another'
     }
   }
   
-  calculateAge('chinchilla', 32)
+  calculateAge('duck', 22)
