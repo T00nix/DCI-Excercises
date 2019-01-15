6de27c08226496ff79febe5a41dcb03ba1fdd3e0
Brian <bredow.brian@gmail.com>
2019-01-15 10:46:46 +0100

    change   from  + to *

--- calculate Age/assets/main.js
+++ calculate Age/assets/main.js
@@ -1,15 +1,15 @@
 function calculateAge (animal, animalAge){
   
     if (animal == 'dog'){
-     let n = (animalAge + 7);
+     let n = (animalAge * 7);
       console.log('Your '+ animal +' is ' + n + ' years old in human years');
       document.getElementById('calcAge').innerHTML='Your '+ animal +' is ' + n + ' years old in human years';
     } else if (animal == 'cow'){
-     let n = animalAge + 14;
+     let n = animalAge * 14;
       console.log('Your cow is ' + n + ' years old in human years');
       document.getElementById('calcAge').innerHTML='Your '+ animal +' is ' + n + ' years old in human years';
     } else if (animal == 'chinchilla'){
-     let n = animalAge + 25;
+     let n = animalAge * 25;
       console.log('Your chinchilla is ' + n + ' years old in human years');
       document.getElementById('calcAge').innerHTML='Your '+ animal +' is ' + n + ' years old in human years';
     } else {
@@ -18,4 +18,4 @@ function calculateAge (animal, animalAge){
     }
   }
   
-  calculateAge('duck', 22)
+  calculateAge('cow', 10)
